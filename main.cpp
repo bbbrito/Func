070a8ae12cc2543fab2e5a999367363e84857482
Brynner Brito <brynner_engc@hotmail.com>
2017-11-07 13:10:27 -0300

    Cria a função cubo com parâmetros passados por valor e por referência

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <stdlib.h>
+#include <iostream>
+using std::cout;
+using std::cin;
 
-int cubo( int );
+int cuboPorValor( int );
+void cuboPorReferencia (int &); /* parâmetros serão passados por referência */
+
 int main()
 {
     int a = 10;
-    int resultado;
-    resultado = cubo( a );
+    cout << "O valor original de a eh: " << a;
+    cout << '\n';
+    a = cuboPorValor( a );
+    cout << "O cubo do numero eh: " << a;
+    cout << '\n';
+
+	cout << "O valor original de a eh: " << a;
+	cout << '\n';
+	cuboPorReferencia(a);
+	cout << "O cubo do numero eh: " << a;
 
     return 0;
 }
-int cubo( int x )
+
+int cuboPorValor( int x )
 {
-    return x*x*x;
+    return x*x*x; /* eleva ao cubo a variável local a */
+}
+
+void cuboPorReferencia(int &y)
+{
+    int a = y;
+    y = a * a * a; /* eleva ao cubo a variável local a */
 }
